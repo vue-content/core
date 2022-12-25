dabdbc6b04b2b0698c0f80ed264c18e7f675fbfa
Robert Rosman <robert.rosman@zmartagroup.com>
2022-12-25 07:07:56 +0100

    feat: moved example to it's own project

--- src/example/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import content from './content.json'
-import App from './App.vue'
-import { VueContent } from '../plugin/VueContent'
-
-const app = createApp(App)
-app.use(VueContent, { content })
-app.mount('#app')
