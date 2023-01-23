74e3cb6d5330e3ed1dc8fd82d8f7551624ccb859
Robert Rosman <robert.rosman@zmartagroup.com>
2023-01-23 14:55:52 +0100

    fix: export some more stuff

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+export type { ContentSource } from "./plugin/ContentSource";
+
 export { default as ContentBlock } from "./components/ContentBlock.vue";
 export { default as ContentList } from "./components/ContentList.vue";
 export { VueContent } from "./plugin/VueContent";
+export { replaceVariables } from "./utils/replaceVariables"
+export { sanitize } from "./utils/sanitize"
