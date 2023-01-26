b5d4e8e15f4639a6e053d0e91410c7bc938813c3
Robert Rosman <robert.rosman@zmartagroup.com>
2023-01-26 14:36:36 +0100

    fix: export sources from plugin

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,7 @@ export type { ContentSource } from "./plugin/ContentSource";
 export { default as ContentBlock } from "./components/ContentBlock.vue";
 export { default as ContentList } from "./components/ContentList.vue";
 export { VueContent } from "./plugin/VueContent";
+export { InMemorySource } from "./plugin/InMemorySource";
+export { LocalizedInMemorySource } from "./plugin/LocalizedInMemorySource";
 export { replaceVariables } from "./utils/replaceVariables"
 export { sanitize } from "./utils/sanitize"
