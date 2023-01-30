34eab5ec81ccdd60ccfd748224b1e84a4786e840
Robert Rosman <robert.rosman@zmartagroup.com>
2023-01-30 21:27:50 +0100

    feat: export Block

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ export type { ContentSource } from "./plugin/ContentSource";
 export { default as ContentBlock } from "./components/ContentBlock.vue";
 export { default as ContentList } from "./components/ContentList.vue";
 export { VueContent } from "./plugin/VueContent";
+export { Block } from "./plugin/Block";
 export { InMemorySource } from "./plugin/InMemorySource";
 export { LocalizedInMemorySource } from "./plugin/LocalizedInMemorySource";
 export { replaceVariables } from "./utils/replaceVariables"
