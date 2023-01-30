1390f5feaa5b5cb8b7a7a81b1e01ad3ca213e145
Robert Rosman <robert.rosman@zmartagroup.com>
2023-01-30 21:52:00 +0100

    refactor: replaced dompurify with xss for smoother server experience

--- src/utils/sanitize.ts
+++ src/utils/sanitize.ts
@@ -1,12 +1,25 @@
-import DOMPurify from "isomorphic-dompurify"
+import xss, { IFilterXSSOptions } from "xss"
 
 interface SanitizeOptions {
     tags?: string[]
 }
 
 export const sanitize = (text: string, { tags }: SanitizeOptions = {}) => {
-    if (tags) {
-        return DOMPurify.sanitize(text, { ALLOWED_TAGS: tags })
+    const fullWhiteList = (xss as any).whiteList
+    const whiteList = tags === undefined
+        ? fullWhiteList
+        : Object.fromEntries(
+            Object.entries(fullWhiteList).filter(
+                ([key, val]) => tags.includes(key)
+            )
+        )
+    const options: IFilterXSSOptions = {
+        whiteList,
+        stripIgnoreTag: true, // filter out all HTML not in the whitelist
+        stripIgnoreTagBody: ['script'] // the script tag is a special case, we need to filter out its content
     }
-    return DOMPurify.sanitize(text)
+    if (tags) {
+        return xss(text, options)
+    }
+    return xss(text, options)
 }
