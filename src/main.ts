ca7ba4f6db7a52e78070c2f6795a79400f0b5eec
Robert Rosman <robert.rosman@zmartagroup.com>
2023-02-19 21:16:21 +0100

    fix: export useContent composable

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ export type { ContentSource } from "./plugin/ContentSource";
 
 export { default as ContentBlock } from "./components/ContentBlock.vue";
 export { default as ContentList } from "./components/ContentList.vue";
+export { useContent } from './composables/useContent'
 export { VueContent } from "./plugin/VueContent";
 export { Block } from "./plugin/Block";
 export { InMemorySource } from "./plugin/InMemorySource";
