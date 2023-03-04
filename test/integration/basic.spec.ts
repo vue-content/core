4b65bc342fdb42d45a98128019e44b5dec2031e6
Robert Rosman <robert.rosman@zmartagroup.com>
2023-03-04 07:28:49 +0100

    test: enabled that locale switch test again

--- test/integration/basic.spec.ts
+++ test/integration/basic.spec.ts
@@ -14,15 +14,15 @@ test('should substitute variables reactively', async ({ page }) => {
   await expect(button).toContainText(/count is 2/)
 })
 
-test.skip('should switch locale', async ({ page }) => {
+test('should switch locale', async ({ page }) => {
   await page.goto('/')
 
   await page.getByText(/^se$/).click()
-  const currentLocaleSe = await page.getByText('Vald översättning')
+  const currentLocaleSe = page.getByText('Vald översättning')
   await expect(currentLocaleSe).toContainText(/Vald översättning: se/)
 
   await page.getByText(/^en$/).click()
-  const currentLocaleEn = await page.getByText('Current locale')
+  const currentLocaleEn = page.getByText('Current locale')
   await expect(currentLocaleEn).toContainText(/Current locale: en/)
 })
 
