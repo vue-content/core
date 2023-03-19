5ff42725aecb2cc0a13cb8fea613449a850ff5cb
Robert Rosman <robert.rosman@zmartagroup.com>
2023-03-19 07:11:14 +0100

    fix: export implements-functions

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-export type { ContentSource } from './plugin/ContentSource'
-
 export { default as ContentBlock } from './components/ContentBlock.vue'
 export { useContent } from './composables/useContent'
 export { VueContent } from './plugin/VueContent'
-export type { Block } from './plugin/Block'
 export { InMemorySource } from './plugin/InMemorySource'
 export { LocalizedInMemorySource } from './plugin/LocalizedInMemorySource'
+export {
+  implementsContentSource,
+  implementsLocalizedContentSource
+} from './plugin/ContentSource'
 export { replaceVariables } from './utils/replaceVariables'
 export { sanitize } from './utils/sanitize'
+export { extendPromise } from './utils/extendPromise'
+
+export type { ContentSource } from './plugin/ContentSource'
+export type { Block } from './plugin/Block'
