847e2c95c824ded5f306b075df0e556d586b34b3
Robert Rosman <robert.rosman@zmartagroup.com>
2023-04-24 21:38:25 +0200

    type: added ContentList to GlobalComponents

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,9 @@
 import type ContentBlock from './components/ContentBlock.vue'
+import type ContentList from './components/ContentList.vue'
 
 declare module 'vue' {
   interface GlobalComponents {
     ContentBlock: typeof ContentBlock
+    ContentList: typeof ContentList
   }
 }
